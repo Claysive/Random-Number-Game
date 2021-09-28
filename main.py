48c11a60cee486fb4057fce95b164071a2f4519b
Clay Holloway <Claysivet23@gmail.com>
2021-09-28 05:28:59 +0000

    Added some comments/author/description. Also added a short TODO

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import random
+#author: Claysive
+#description: just a simple random number guessing game
+#TODO: update to include a user number and have the computer guess it. should include an option of which game to play
 
+import random #imports the random module to generate a random number using 'randint'
+
+#function to generate a random number btwn one and a hundred. may update it to allow the number range to be user defined
 def get_guess():
     guess = random.randint(1, 100)
     return guess
 
+#this is the main function. will hopefully update soon with the todo from above
 def player_guess():
     comp_guess = get_guess()
     playerGuess = 0
@@ -22,9 +28,12 @@ def player_guess():
         if guessCounter > 5:
             print(f"Better luck next time peasant! My number was {comp_guess}!")
 
+#set the title of the game
 title = f"Welcome to the number guessing game! You'll have 5 trys to guess my number.\n" \
 f"Are you smarter than me? Let's find out! The number is between 1 and 100. Good Luck!"
 
+#print the title of the game
 print(title)
 
+#run the main function of the game
 player_guess()
